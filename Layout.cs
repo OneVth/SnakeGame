be5b2a708d13184eec55ce2997c9be83622ebcce
OneVth <onebrotravel@gmail.com>
2024-07-07 17:30:23 +0900

    Update Layout class

--- Layout.cs
+++ Layout.cs
@@ -30,13 +30,30 @@ namespace SnakeGame
             }
         }
 
-        public void Render(Player player)
+        public void Render(Player player, Item item)
         {
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-                    Console.Write(GameBoard[i][j]);
+                    if (player.Body[0].x == item.PosX && player.Body[0].y == item.PosY)
+                    {
+                        player.EatItem();
+                        item.Spawn(this);
+                    }
+
+                    if (player.IsAtPosition(j, i))
+                    {
+                        Console.Write("■");
+                    }          
+                    else if(item.PosX == j && item.PosY == i)
+                    {
+                        Console.Write("▣");
+                    }
+                    else
+                    {
+                        Console.Write("□");
+                    }
                 }
                 Console.WriteLine();
             }
