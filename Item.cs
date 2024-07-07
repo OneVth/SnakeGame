1b8345f312117a75ac62507943a02edb88b08a5b
OneVth <onebrotravel@gmail.com>
2024-07-07 17:30:37 +0900

    Implement Item class

---
+++ Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    internal class Item
+    {
+        int posX;
+        int posY;
+        bool isSpawned = false;
+        
+        public int PosX { get { return posX; } }
+        public int PosY { get { return posY; } }
+
+        Random rand;
+
+        public void Spawn(Layout layout)
+        {
+            rand = new Random();
+
+            posX = rand.Next(layout.Width);
+            posY = rand.Next(layout.Height);
+            ChangeState();
+        }
+
+        public bool IsSpawned()
+        {
+            return isSpawned;
+        }
+
+        public void ChangeState()
+        {
+            isSpawned = !isSpawned;
+        }
+    }
+}
